6c83b08ab800d3f5f41cf1320d9521bd5a65b999
Fauzan Maizardi <maizardifauzan@gmail.com>
2022-09-19 23:41:36 +0700

    tambahan di halaman form penetapan penguji TA dan seminar

---
+++ app/src/main/java/com/example/a8/form_penetapan_penguji_TA_status_hasil_Activity.java
@@ -0,0 +1,14 @@
+package com.example.a8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class form_penetapan_penguji_TA_status_hasil_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_penetapan_penguji_ta_status_hasil);
+    }
+}
