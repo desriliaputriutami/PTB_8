773bb175089d7c78686216bd438f4f2f36cfff17
desriliaputriutami <desriliaputriutami@gmail.com>
2022-09-19 21:30:34 +0700

    login & dashboard

---
+++ app/src/main/java/com/example/a8/DetailHalamanSeminar.java
@@ -0,0 +1,14 @@
+package com.example.a8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailHalamanSeminar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_halaman_seminar);
+    }
+}
