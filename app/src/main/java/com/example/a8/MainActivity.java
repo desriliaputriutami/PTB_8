fae7bbdd269ac1e725a06ab003c9c474b0b028ba
Fauzan Maizardi <maizardifauzan@gmail.com>
2022-10-14 17:30:11 +0700

    bikin  recycleview buat halaman List permintaan TA

--- app/src/main/java/com/example/a8/MainActivity.java
+++ app/src/main/java/com/example/a8/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.a8;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        Button button1 = findViewById(R.id.button1);
+        Button button1 = findViewById(R.id.cuss_ke_detail_TA);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/a8/models/Data.java
@@ -0,0 +1,40 @@
+package com.example.a8.models;
+
+public class Data {
+
+    String gambar;
+    String nama;
+    String judul;
+
+    public Data(){ }
+
+    public Data(String gambar, String nama, String judul) {
+        this.gambar = gambar;
+        this.nama = nama;
+        this.judul = judul;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(String gambar) {
+        this.gambar = gambar;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+}
