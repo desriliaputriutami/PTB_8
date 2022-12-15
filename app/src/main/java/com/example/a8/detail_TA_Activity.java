d452458207e8f2bb18d61cd83e16c442ca65e137
fauzan <maizardifauzan@gmail.com>
2022-12-15 21:52:53 +0700

    fix dikit di detail_TA_Activity.java

--- app/src/main/java/com/example/a8/detail_TA_Activity.java
+++ app/src/main/java/com/example/a8/detail_TA_Activity.java
@@ -2,13 +2,34 @@ package com.example.a8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class detail_TA_Activity extends AppCompatActivity {
+    String namaData;
+    String judulData;
+
+    TextView nama_mahasiswa;         //id dari nama yang ada di halaman detail mahasiswa TA
+    TextView isi_judul_tugas_akhir;   //id dari judul yang ada di halaman detail mahasiswa TA
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_ta);
+
+        Intent detailIntent = getIntent();
+        if (detailIntent != null) {
+            namaData = detailIntent.getStringExtra("nama_mahasiswa");
+            judulData = detailIntent.getStringExtra("isi_judul_tugas_akhir");
+
+            nama_mahasiswa = findViewById(R.id.nama_mahasiswa);
+            isi_judul_tugas_akhir = findViewById(R.id.isi_judul_tugas_akhir);
+
+            nama_mahasiswa.setText(namaData);
+            isi_judul_tugas_akhir.setText((judulData));
+        }
+
     }
+
 }
