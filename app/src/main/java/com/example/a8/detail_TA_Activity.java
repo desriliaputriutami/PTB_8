59ecda18369bbfa69c6d83a8fbf2328b27ba790c
fauzan <maizardifauzan@gmail.com>
2022-12-16 16:16:19 +0700

    nambahin halaman logbook

--- app/src/main/java/com/example/a8/detail_TA_Activity.java
+++ app/src/main/java/com/example/a8/detail_TA_Activity.java
@@ -1,7 +1,12 @@
 package com.example.a8;
 
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class detail_TA_Activity extends AppCompatActivity {
